40f5f3d43c0ce4ddeb5c19fb1274b4e62c10b4f2
Volodymyr Doskochinskiy <doskovol1@gmail.com>
2022-04-07 12:34:32 +0300

    Replaced legacy code in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,20 +14,14 @@ export default {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
+        loader: 'babel-loader'
       }
     ]
   },
   resolve: {
     extensions: ['.js', '.json'],
     alias: {
-      "@cmps": path.resolve(__dirname, 'src/components')
+      '@cmps': path.resolve(__dirname, 'src/components')
     }
   }
 }
